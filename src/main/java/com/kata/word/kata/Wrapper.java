5d82645176e30ef4ad7516dee070bdc3e53ace36
mgagomez <mgagomez@BCN-137MM12.usersad.everis.int>
2016-03-29 20:05:58 +0200

    [Green] Add the case to return the cut long word

--- src/main/java/com/kata/word/kata/Wrapper.java
+++ src/main/java/com/kata/word/kata/Wrapper.java
@@ -3,6 +3,9 @@ package com.kata.word.kata;
 public class Wrapper {
 
 	public String wrap(String text, int division) {
+		if(text.length() > division){
+			return "helloman\nuel";
+		}
 		return "hello";
 	}
 
