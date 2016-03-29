6dc71a78c40c03982c994e957794813740d5095c
mgagomez <mgagomez@BCN-137MM12.usersad.everis.int>
2016-03-29 19:24:08 +0200

    Prepare TODO LIST

---
+++ src/main/test/com/kata/word/kata/WordWrapTest.java
@@ -0,0 +1,21 @@
+package com.kata.word.kata;
+
+/*
+ * TODO LIST
+ * - Recortar por espacio
+ * 		hola manuel,8 -> hola\nmanuel
+ * 		hola morata manuel, 8 -> hola\nmorata\nmanuel
+ * 
+ * - Recortar palabra simple
+ * 		hello,8 --> hello
+ * 		francisco,3 -> fra\nnci\n...
+ * 
+ * - Recortar combinado
+ * 		hello francisco, 3
+ * 		hello\nfrancisco->hel\no\nfra\n\nci...
+ *
+ */
+public class WordWrapTest {
+
+	
+}
