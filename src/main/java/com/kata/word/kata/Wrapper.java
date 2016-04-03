c06662ca1f068290d33e8aa217e0df2d85815e57
mgagomez <mgagomez@BCN-137MM12.usersad.everis.int>
2016-04-03 18:35:32 +0200

    [Green] If the text contains blanks, replace it by \n

--- src/main/java/com/kata/word/kata/Wrapper.java
+++ src/main/java/com/kata/word/kata/Wrapper.java
@@ -3,8 +3,12 @@ package com.kata.word.kata;
 public class Wrapper {
 
 	public String wrap(String text, int division) {
-		if(text.length() > division){
-			return text.substring(0,division)+ "\n"  + text.substring(division);
+		if (text.length() > division) {
+			if (text.contains(" ")) {
+				return text.replace(" ","\n");
+			} else {
+				return text.substring(0, division) + "\n" + text.substring(division);
+			}
 		}
 		return text;
 	}
